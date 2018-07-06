9e1625b0670f56ff069972d67d8de3b93675e290
Alex <16453186+alextsk@users.noreply.github.com>
2018-07-06 17:58:43 +0700

    html renderer tested and applied

--- src/html-renderer.js
+++ src/html-renderer.js
@@ -1,27 +1,33 @@
+import {getCell} from "./logic.js"
+
 function makeTableCell (grid, x, y) {
-  return `
-    <td>${getCell(grid, x, y)}</td>
-  `
+  return  `<td>${getCell(grid, x, y)}</td>`
 }
-function makeTableRow (row, y) {
-  return ` 
-    <tr>
-      ${row.map((cell, x) => makeTableCell(cell, x, y)).join("")}
-    </tr>
-    `
+
+function makeTableRow (grid, y) {
+  return ( 
+    `<tr>
+      ${ grid.map((cell, x) => makeTableCell(grid, x+1, y)).join("") }
+    </tr>`
+    )
 }
+
 function makeTable (grid) {
   return ` 
-  <table onclick=delegatedHandler>
-    ${grid.map(row => makeTableRow(row)).join("")}
+  <table>
+    <tbody>
+      ${grid.map((row, y) => makeTableRow(grid, y+1)).join("")}
+    </tbody>
   </table>
   `
 }
 
-function renderHtml(grid, kill, revive) {
+function renderHtml(grid) {
   return `
     <div>
       ${makeTable(grid)}
     </div>
     `
 }
+
+export {makeTableCell, makeTableRow, makeTable, renderHtml}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import {generateField} from "./logic.js"
+import {partial} from "./utils.js"
+import {generateField, createGrid, updateState} from "./logic.js"
 import {renderHtml} from "./html-renderer.js"
 
 const draw = (container, render, grid) => {
-  container.innerHtml = render(grid)
+  container.innerHTML = render(grid)
 }
 
 
 
 function main() {
-  const g = generateField(createGrid(5,5))
+  let grid = generateField(createGrid(10,10))
   const gameContainer = document.querySelector("#game")
-  const drawApplied = partial(draw, gameContainer, renderHtml)
-  setTimeout( () => drawApplied(g), 1000)
+
+  const drawHtmlInContainer = partial(draw, gameContainer, renderHtml)
+  drawHtmlInContainer(grid)
   
+  setInterval( () => {
+    const ngrid = updateState(grid)
+    drawHtmlInContainer(ngrid)
+    grid = ngrid
+  }, 1000)
 }
+main()
 
