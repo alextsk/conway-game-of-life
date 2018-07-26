bc0ef9306d0776a0d531e961351cab8fab82149e
Alex <16453186+alextsk@users.noreply.github.com>
2018-07-26 15:56:28 +0700

    index linted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import "./styles.css";
-import { partial } from './utils.js'
-import { renderField, renderControls } from './html-renderer.js'
+import './styles.css';
+import { renderField, renderControls } from './html-renderer';
 import {
-cellClickHandler, resetHandler, widthHandler, heightHandler, speedHandler, playHandler, initField, initControls } from './events.js'
-import { modelInit, updateState, toggleCell } from './state.js'
+  cellClickHandler,
+  resetHandler,
+  widthHandler,
+  heightHandler,
+  speedHandler,
+  playHandler,
+  initField,
+  initControls,
+} from './events';
+import { modelInit, updateState, toggleCell } from './state';
 
 
 function main(fieldWidth, fieldHeight) {
@@ -13,17 +20,19 @@ function main(fieldWidth, fieldHeight) {
 
   model.addObserver('toggle', (data) => {
     model.setGrid(toggleCell(model.getGrid(), data.x, data.y));
-  });  
-  model.addObserver('update', (data) => {
+  });
+
+  model.addObserver('update', () => {
     model.setGrid(updateState(model.getGrid()));
   });
-  model.addObserver('redraw', (data) => {
+
+  model.addObserver('redraw', () => {
     gameContainer.innerHTML = renderField(model.getGrid(), () => 'js-cell');
   });
 
-  model.addObserver('diff', (data) => {
-    const diff = model.diff();
-    diff.map((diff) => {
+  model.addObserver('diff', () => {
+    const modelDiff = model.diff();
+    modelDiff.forEach((diff) => {
       const element = gameContainer.querySelector(`#x${diff.x + 1}y${diff.y + 1}`);
       element.classList.toggle('alive');
     });
@@ -32,54 +41,54 @@ function main(fieldWidth, fieldHeight) {
   model.addObserver('controls', (components) => {
     controlsContainer.innerHTML = renderControls(components);
   });
-  initControls(
-    {
-      model,
-      container: controlsContainer,
-      components: {
-        reset: {
-          selector: '#reset',
-          handler: resetHandler,
-          title: 'Reset'
-        },
-        play: {
-          selector: '#run',
-          handler: playHandler,
-          title: 'Pause'
-        },
-        speed: {
-          selector: '#speed',
-          auxSelector: '#speed-number',
-          title: 'Speed',
-          handler: speedHandler,
-          reporter: val => `${(1000 / val).toFixed(2)   } renders/sec`,
-          minVal: 100,
-          maxVal: 2000,
-          initVal: model.getSpeed(),
-        },
-        width: {
-          selector: '#width',
-          auxSelector: '#width-number',
-          title: 'Width',
-          handler: widthHandler,
-          reporter: val => `${val  } cells`,
-          minVal: 10,
-          maxVal: 200,
-          initVal: model.getWidth(),
-        },
-        height: {
-          selector: '#height',
-          auxSelector: '#height-number',
-          title: 'Height',
-          handler: heightHandler,
-          reporter: val => `${val  } cells`,
-          minVal: 10,
-          maxVal: 200,
-          initVal: model.getHeight(),
-        }
-      }
-    }
-  );
+  const config = {
+    model,
+    container: controlsContainer,
+    components: {
+      reset: {
+        selector: '#reset',
+        handler: resetHandler,
+        title: 'Reset',
+      },
+      play: {
+        selector: '#run',
+        handler: playHandler,
+        title: 'Pause',
+      },
+      speed: {
+        selector: '#speed',
+        auxSelector: '#speed-number',
+        title: 'Speed',
+        handler: speedHandler,
+        reporter: val => `${(1000 / val).toFixed(2)} renders/sec`,
+        minVal: 100,
+        maxVal: 2000,
+        initVal: model.getSpeed(),
+      },
+      width: {
+        selector: '#width',
+        auxSelector: '#width-number',
+        title: 'Width',
+        handler: widthHandler,
+        reporter: val => `${val} cells`,
+        minVal: 10,
+        maxVal: 200,
+        initVal: model.getWidth(),
+      },
+      height: {
+        selector: '#height',
+        auxSelector: '#height-number',
+        title: 'Height',
+        handler: heightHandler,
+        reporter: val => `${val} cells`,
+        minVal: 10,
+        maxVal: 200,
+        initVal: model.getHeight(),
+      },
+    },
+  };
+
+  initControls(config);
 
   initField({
     model,
