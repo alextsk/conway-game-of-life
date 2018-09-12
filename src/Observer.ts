4041e644319de1bd2a10d57ea81be76ce646709f
Alex <16453186+alextsk@users.noreply.github.com>
2018-09-12 23:20:49 +0700

    observer factored out to separate class

--- src/Observer.ts
+++ src/Observer.ts
@@ -1,20 +1,21 @@
-import Msg from './messages.ts';
+import Messages from './Messages';
+type Payload = any;
 
 class Observer{
-    private observers = [];
-  
-    broadcast(type: Msg, data?) {
-      this.observers.forEach(observer => observer.type === type && observer.handler(data));
-    }
-  
-    addObserver(type: Msg, handler) {
-      this.observers.push({ type, handler });
-    }
-    
-    removeObserver(handler) : void {
-      let index = this.observers.findIndex(obj => obj.handler === handler)
-      this.observers.splice(index, 1)
-    }
+  private observers = [];
+
+  broadcast(type: Messages, data?: Payload) {
+    this.observers.forEach(observer => observer.type === type && observer.handler(data));
   }
+
+  addObserver(type: Messages, handler: (data?: Payload) => void) {
+    this.observers.push({ type, handler });
+  }
+
+  removeObserver(handler: (data?: Payload) => void) : void {
+    const index = this.observers.findIndex(obj => obj.handler === handler);
+    this.observers.splice(index, 1);
+  }
+}
 
 export default Observer;
