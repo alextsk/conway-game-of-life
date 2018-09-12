d8020133d4203dfd74fe8b10e9523dc32f5ebcad
Alex <16453186+alextsk@users.noreply.github.com>
2018-09-12 23:23:15 +0700

    messages refactored to typescript

---
+++ src/Messages.ts
@@ -0,0 +1,15 @@
+enum Messages{
+  TOGGLE= 'toggle',
+  UPDATE= 'update',
+  REDRAW= 'redraw',
+  CONTROLS= 'controls',
+  DIFF= 'diff',
+  SPEED= 'speed',
+  WIDTH= 'width',
+  HEIGHT= 'height',
+  RESET= 'reset',
+  PLAYPAUSE= 'running',
+  GAMESTATUS= 'gamestatus',
+}
+
+export default Messages;
