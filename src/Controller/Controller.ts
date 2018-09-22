422908c37b4bc36ad7ac6ca44d61de284f378f91
Alex <16453186+alextsk@users.noreply.github.com>
2018-09-22 17:12:54 +0700

    controller fixed

--- src/Controller/Controller.ts
+++ src/Controller/Controller.ts
@@ -1,64 +1,50 @@
-import Messages from '../Utilities/Messages';
+import {viewMessages, modelMessages} from '../Utilities/Messages';
 import Model from '../Model/Model';
 import View from '../View/View';
 
-class Controller {
+class Controller{
   private views = [];
   private models = [];
-  
-  resetObservers() {
-    this.views.forEach((view) => {
-      [Messages.UPDATE_STATE, 
-        Messages.UPDATE_WIDTH,
-        Messages.TOGGLE_CELL,
-        Messages.UPDATE_HEIGHT,
-        Messages.RESET,
-      ].forEach(message => 
-        view.addObserver(message, payload => 
-          this.models.forEach(model => model.broadcast(message, payload)),
-        ),
-        );
-    });
 
-    this.models.forEach((model) => {
-      [Messages.STATE_UPDATED, 
-        Messages.STATUS_CHANGED]
-      .forEach(message =>
-        model.addObserver(message, (payload) => {
-          this.views.forEach(view => view.broadcast(message, payload));
-        })
-      );
-      // model.addObserver(Messages.STATE_UPDATED, (grid) => {
-      //   this.views.forEach(view => view.broadcast(Messages.STATE_UPDATED, grid));
-      // });
-  
-      // model.addObserver(Messages.STATUS_CHANGED, (stability) => {
-      //   this.views.forEach(view => view.broadcast(Messages.STATUS_CHANGED, stability));
-      // });
-    });
+  private subcribeModelsToView(view: View): void {
+    viewMessages.forEach(message => 
+      view.addObserver(message, (payload) => 
+        this.models.forEach(model => 
+          model.broadcast(message, payload)
+        )
+      )
+    )
   }
 
-  addView(view) {
+  private subscribeViewsToModel(model: Model): void {
+    modelMessages.forEach(message => 
+      model.addObserver(message, (payload) => 
+        this.views.forEach(view => 
+          view.broadcast(message, payload)
+        )
+      )
+    )
+  }
+
+  public addView(view: View): View {
     this.views.push(view);
-    this.resetObservers();
+    this.subcribeModelsToView(view);
     return view;
   }
 
-  removeView(viewToRemove) {
+  public removeView(viewToRemove : View): void {
     this.views = this.views.filter(view => view !== viewToRemove);
-    this.resetObservers();
   }
 
-  addModel(model) {
+  public addModel(model: Model): Model {
     this.models.push(model);
-    this.resetObservers();
+    this.subscribeViewsToModel(model);
     return model;
   }
 
-  removeModel(modelToRemove) {
+  public removeModel(modelToRemove: Model): void {
     this.models = this.models.filter(model => model !== modelToRemove);
-    this.resetObservers();
   }
-
 }
+
 export default Controller;
