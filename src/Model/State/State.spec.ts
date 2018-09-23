999524bef8fb6f62d179712e1790b6a6220ff491
Alex <16453186+alextsk@users.noreply.github.com>
2018-09-23 21:04:36 +0700

    state tests fixed

--- src/Model/State/State.spec.ts
+++ src/Model/State/State.spec.ts
@@ -1,28 +1,18 @@
 import State from './State';
 
-describe('generateField', () => {
-  it('should not mutate an existing grid', () => {
-    const currentState = [[0, 0, 0],
-                          [1, 1, 1],
-                          [0, 0, 0]];
-    const newState = new State(3, 3, 3);
-    newState.setGrid(currentState); 
-    expect(currentState).toEqual([[0, 0, 0],
-                                  [1, 1, 1],
-                                  [0, 0, 0]]);
-    expect(newState.getGrid()).not.toBe(currentState);
-  });
+describe('resetGrid', () => {
 
   it('should create a grid of the same dimensions as given', () => {
-    const currentState = (new State(9, 5, 3)).getGrid();
-    const newState = (new State(3, 3, 3)).setGrid(currentState);
-    expect(newState.length).toBe(currentState.length);
-    expect(newState[0].length).toBe(currentState[0].length);
+    const newState = new State(3, 3);
+    const dimensions = { width: newState.getGrid().length, height: newState.getGrid()[0].length };
+    newState.resetGrid();
+    expect(newState.getGrid().length).toBe(dimensions.width);
+    expect(newState.getGrid()[0].length).toBe(dimensions.height);
   });
 
   describe("should randomly populate a grid with 0's and 1's", () => {
     it("should not put anything but 1's or 0's ", () => {
-      const newState = (new State(9, 9, 3)).getGrid();
+      const newState = (new State(9, 9)).getGrid();
       const noOtherValues = newState
       .reduce(
         (acc, row) => {
@@ -37,24 +27,17 @@ describe('generateField', () => {
     });
 
     it("it should put at least one '1' in a reasonably sized field",  () => {
-      const newState = (new State(9, 9, 3)).getGrid(); 
+      const newState = (new State(9, 9)).getGrid();
       const anyOnes = newState.some(row => row.some(cell => cell === 1));
       expect(anyOnes).toBe(true);
     });
   });
-  
 });
 
-xdescribe('size', () => {
+describe('size', () => {
   it('should create a grid of given size', () => {
-    const grid = (new State(5, 7, 3)).getGrid(); 
+    const grid = (new State(5, 7)).getGrid();
     expect(grid.length).toBe(7);
     expect(grid[6].length).toBe(5);
-  });
-
-  it("should populate created grid with 0's", () => {
-    const grid = (new State(5, 7, 3)).createGrid(); 
-    expect(grid[0][0]).toBe(0);
-    expect(grid[6][4]).toBe(0);
   });
 });
