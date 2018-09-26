7fd23f81966e7e7b6fc25dd579b418008d943232
Alex <16453186+alextsk@users.noreply.github.com>
2018-09-26 21:17:03 +0700

    fixes on view

--- src/View/View.ts
+++ src/View/View.ts
@@ -3,8 +3,9 @@ import Config from './Config';
 import Observer from '../Utilities/Observer';
 import Template from './Template/Template';
 import IView from './IView';
-import bind from 'bind-decorator';
+import bindDecorator from 'bind-decorator';
 import Animate from '../Utilities/Animate';
+import { ComponentT, ConfigT } from '../Utilities/Types';
 
 class View extends Observer implements IView{
   private stable: boolean;
@@ -14,10 +15,10 @@ class View extends Observer implements IView{
   private controlsContainer: HTMLElement;
   private fieldContainer :HTMLElement;
   private playButton: HTMLElement;
-  private config;
+  private config: ConfigT;
   private animation: Animate;
 
-  constructor(rootSelector) {
+  constructor(rootSelector: string) {
     super();
     this.config = Config(rootSelector);
     this.animation = new Animate(this.config.controls.components.speed.initVal);
@@ -29,44 +30,64 @@ class View extends Observer implements IView{
 
   private initHTML(): void {
     this.template = new Template(this.config);
-    const gameContainer = document.querySelector(this.config.selector);
-    gameContainer.innerHTML = (this.template.renderControls()) + (this.template.renderField());
-    this.controlsContainer = gameContainer.querySelector(this.config.controls.selector);
-    this.fieldContainer = gameContainer.querySelector(this.config.field.wrapperSelector);
-    this.statusMessage = gameContainer.querySelector(this.config.controls.components.message.selector);/*tslint:disable-line */
+    const {
+      selector,
+      controls: {
+        selector: controlsSelector,
+        components: {
+          message: {
+            selector: messageSelector,
+          },
+        },
+      },
+      field: { wrapperSelector },
+    } = this.config;
+    const gameContainer = document.querySelector(selector);
+    gameContainer.innerHTML = `${(this.template.renderControls())}${(this.template.renderField())}`;
+    this.controlsContainer = gameContainer.querySelector(controlsSelector);
+    this.fieldContainer = gameContainer.querySelector(wrapperSelector);
+    this.statusMessage = gameContainer.querySelector(messageSelector);
   }
 
   private initObservers(): void {
-    this.addObserver(Messages.UPDATE_SPEED, (value) => { 
-      this.animation.changeSpeed(value);
-    });
-
-    this.addObserver(Messages.STATE_UPDATED, (grid) => { 
-      this.fieldContainer.innerHTML = this.template.makeTable(grid);
-      console.log(this.template.renderField(grid));
-    });
-
-    this.addObserver(Messages.STATUS_CHANGED, (stability) => {
-      this.stable = stability;
-    });
-
-    this.addObserver(Messages.RUNNING_CHANGED, () => {
-      this.running = !this.running;
-      const playConfig = this.config.controls.components.play;
-      this.playButton.innerHTML = playConfig[this.running ? 'title' : 'titlePaused'];
-    });
+    this.addObserver(Messages.UPDATE_SPEED, this.onUpdateSpeed);
+    this.addObserver(Messages.STATE_UPDATED, this.onStateUpdated);
+    this.addObserver(Messages.STATUS_CHANGED, this.onStatusChanged);
+    this.addObserver(Messages.RUNNING_CHANGED, this.onRunningChanged);
+  }
+  
+  @bindDecorator
+  private onRunningChanged() {
+    this.running = !this.running;
+    const playConfig = this.config.controls.components.play;
+    this.playButton.innerHTML = playConfig[this.running ? 'title' : 'titlePaused'];
   }
 
-  private initControls(): void {
-    const components = this.config.controls.components;
-    this.setSliderEvents(this.controlsContainer, components.speed, this.handleSpeedSliderChange);
-    this.setSliderEvents(this.controlsContainer, components.width, this.handleWidthSliderChange);
-    this.setSliderEvents(this.controlsContainer, components.height, this.handleHeightSliderChange);
+  @bindDecorator
+  private onStatusChanged(stability) {
+    this.stable = stability;
+  }
 
-    this.playButton = this.controlsContainer.querySelector(`${components.play.selector}--${components.play.modifier}`);
+  @bindDecorator
+  private onStateUpdated(grid)  {
+    this.fieldContainer.innerHTML = this.template.makeTable(grid);
+  }
+
+  @bindDecorator
+  private onUpdateSpeed(value) {
+    this.animation.changeSpeed(value);
+  }
+  
+  private initControls(): void {
+    const { controls: { components: { speed, width, height, play, reset } } } = this.config;
+    this.setSliderEvents(this.controlsContainer, speed, this.handleSpeedSliderChange);
+    this.setSliderEvents(this.controlsContainer, width, this.handleWidthSliderChange);
+    this.setSliderEvents(this.controlsContainer, height, this.handleHeightSliderChange);
+
+    this.playButton = this.controlsContainer.querySelector(`${play.selector}--${play.modifier}`);
     this.playButton.addEventListener('click', this.handlePlayButtonClick);
 
-    const resetBtn = this.controlsContainer.querySelector(`${components.reset.selector}--${components.reset.modifier}`);
+    const resetBtn = this.controlsContainer.querySelector(`${reset.selector}--${reset.modifier}`);
     resetBtn.addEventListener('click', this.handleResetButtonClick);
   }
   
@@ -76,19 +97,22 @@ class View extends Observer implements IView{
     this.animation.run(this.draw);
   }
 
-  private setSliderEvents(container, opts, handler): void {
-    const sel = container.querySelector(`${opts.selector}--${opts.modifier}`);
-    sel.value = opts.initVal;
-    const auxSel = container.querySelector(`${opts.auxSelector}--${opts.modifier}`);
-    this.addObserver(opts.message, (value) => {
-      auxSel.innerHTML = opts.reporter(value);
-      return auxSel.innerHTML;
+  private setSliderEvents(
+      container: HTMLElement,
+      { auxSelector, selector, modifier, reporter, message, initVal }: ComponentT,
+      handler: (event: MouseEvent) => void): void {
+    const inputNode: HTMLInputElement = container.querySelector(`${selector}--${modifier}`);
+    inputNode.value = `${initVal}`;
+    const subtitleNode = container.querySelector(`${auxSelector}--${modifier}`);
+    this.addObserver(message, (value) => {
+      subtitleNode.innerHTML = reporter(value);
+      return subtitleNode.innerHTML;
     });
-    this.broadcast(opts.message, sel.value);
-    sel.addEventListener('change', handler);
+    this.broadcast(message, inputNode.value);
+    inputNode.addEventListener('change', handler);
   }
 
-  @bind
+  @bindDecorator
   private draw(): void {
     if (this.running) {
       const messageConfig = this.config.controls.components.message;
@@ -97,36 +121,37 @@ class View extends Observer implements IView{
     }
   }
 
-  @bind
-  private handleCellClick(event): void {
-    if (event.target.classList.contains(`js-${this.config.field.cellSelector.slice(1)}`)) {
-      const data = event.target.dataset;
+  @bindDecorator
+  private handleCellClick(event: MouseEvent): void {
+    if ((<HTMLInputElement>event.target).classList
+      .contains(`js-${this.config.field.cellSelector.slice(1)}`)) {
+      const data = (<HTMLInputElement>event.target).dataset;
       this.broadcast(Messages.TOGGLE_CELL, data);
     }
   }
 
-  @bind
+  @bindDecorator
   private handleResetButtonClick(): void {
     this.broadcast(Messages.RESET);
     this.broadcast(Messages.UPDATE_STATE);
   }
 
-  @bind
+  @bindDecorator
   private handleSpeedSliderChange(event): void {
     this.broadcast(Messages.UPDATE_SPEED, +event.target.value);
   }
 
-  @bind
+  @bindDecorator
   private handleWidthSliderChange(event): void {
     this.broadcast(Messages.UPDATE_WIDTH, +event.target.value);
   }
 
-  @bind
+  @bindDecorator
   private handleHeightSliderChange(event): void {
     this.broadcast(Messages.UPDATE_HEIGHT, +event.target.value);
   }
 
-  @bind
+  @bindDecorator
   private handlePlayButtonClick(): void {
     this.broadcast(Messages.RUNNING_CHANGED);
   }
